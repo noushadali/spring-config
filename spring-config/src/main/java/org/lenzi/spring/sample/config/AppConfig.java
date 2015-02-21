278ea0ff139bda96b5485789bb4ef823d515aa06
slenzi <slenzi@gmail.com>
2015-02-21 15:03:17 -0500

    component scan tweak

--- spring-config/src/main/java/org/lenzi/spring/sample/config/AppConfig.java
+++ spring-config/src/main/java/org/lenzi/spring/sample/config/AppConfig.java
@@ -2,15 +2,23 @@ package org.lenzi.spring.sample.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Controller;
 
 /**
  * Spring project configuration setup
  * 
+ * Ignore controller beans. Our MVC setup class will load those.
+ * 
+ * @see org.lenzi.spring.sample.config.WebMvcConfig
+ * 
  * @author slenzi
  */
 @Configuration
-@ComponentScan(basePackages = "org.lenzi.spring.sample")
+@ComponentScan(basePackages = "org.lenzi.spring.sample", excludeFilters = {
+		@ComponentScan.Filter(value = Controller.class, type = FilterType.ANNOTATION),
+		@ComponentScan.Filter(value = Configuration.class, type = FilterType.ANNOTATION) })
 @Import({ PropertyConfig.class, PersistenceConfig.class, UploadConfig.class })
 public class AppConfig {
 
